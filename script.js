5fcbd7f5a1bcb9854b59707c9af5e217b0fe1a71
JimenaPerez22 <jimenaperezmorales12@gmail.com>
2024-11-08 10:37:33 -0500

    Updated JAVASCRIPT

--- script.js
+++ script.js
@@ -1,2 +1,127 @@
 // YOUR CODE HERE
 console.log("Script Running.");
+
+//switch 1
+const switch1 = document.querySelector("#switch1");
+
+const change38 = () =>{
+    const lightbulb3 = document.querySelector("#lightbulb3");
+    const lightbulb8 = document.querySelector("#lightbulb8");
+
+    lightbulb3.classList.toggle("active");
+    lightbulb8.classList.toggle("active");
+}
+
+switch1.addEventListener("change", change38);
+
+
+//switch 2
+const switch2 = document.querySelector("#switch2");
+
+const change17 = () =>{
+    const lightbulb1 = document.querySelector("#lightbulb1");
+    const lightbulb7 = document.querySelector("#lightbulb7");
+
+    lightbulb1.classList.toggle("active");
+    lightbulb7.classList.toggle("active");
+}
+
+switch2.addEventListener("change", change17);
+
+//switch 3
+const switch3 = document.querySelector("#switch3");
+
+const change468 = () =>{
+    const lightbulb4 = document.querySelector("#lightbulb4");
+    const lightbulb6 = document.querySelector("#lightbulb6");
+    const lightbulb8 = document.querySelector("#lightbulb8");
+
+    lightbulb4.classList.toggle("active");
+    lightbulb6.classList.toggle("active");
+    lightbulb8.classList.toggle("active");
+}
+
+switch3.addEventListener("change", change468);
+
+//switch 4
+const switch4 = document.querySelector("#switch4");
+
+const change29 = () =>{
+    const lightbulb2 = document.querySelector("#lightbulb2");
+    const lightbulb9 = document.querySelector("#lightbulb9");
+    
+    lightbulb2.classList.toggle("active");
+    lightbulb9.classList.toggle("active");
+}
+
+switch4.addEventListener("change", change29);
+
+//switch 5 *new function
+const switch5 = document.querySelector("#switch5");
+
+const change528 = () =>{
+    const lightbulb5 = document.querySelector("#lightbulb5");
+    const lightbulb2 = document.querySelector("#lightbulb2");
+    const lightbulb8 = document.querySelector("#lightbulb8");
+
+    lightbulb5.classList.toggle("active");
+    lightbulb2.classList.toggle("active");
+    lightbulb8.classList.toggle("active");
+}
+
+switch5.addEventListener("change", change528);
+switch2.addEventListener("change", change528)
+
+//switch 6
+const switch6 = document.querySelector("#switch6");
+
+const change14 = () =>{
+    const lightbulb1 = document.querySelector("#lightbulb1");
+    const lightbulb4 = document.querySelector("#lightbulb4");
+    
+    lightbulb1.classList.toggle("active");
+    lightbulb4.classList.toggle("active");
+}
+
+switch6.addEventListener("change", change14);
+
+//switch 7
+const switch7 = document.querySelector("#switch7");
+
+const change23 = () =>{
+    const lightbulb2 = document.querySelector("#lightbulb2");
+    const lightbulb3 = document.querySelector("#lightbulb3");
+    
+    lightbulb2.classList.toggle("active");
+    lightbulb3.classList.toggle("active");
+}
+
+switch7.addEventListener("change", change23);
+
+//switch 8
+const switch8 = document.querySelector("#switch8");
+
+const change59 = () =>{
+    const lightbulb5 = document.querySelector("#lightbulb5");
+    const lightbulb9 = document.querySelector("#lightbulb9");
+    
+    lightbulb5.classList.toggle("active");
+    lightbulb9.classList.toggle("active");
+}
+
+switch8.addEventListener("change", change59);
+
+//switch 9
+const switch9 = document.querySelector("#switch9");
+
+const change67 = () =>{
+    const lightbulb6 = document.querySelector("#lightbulb6");
+    const lightbulb7 = document.querySelector("#lightbulb7");
+    
+    lightbulb6.classList.toggle("active");
+    lightbulb7.classList.toggle("active");
+}
+
+switch9.addEventListener("change", change67);
+
+//master switch
